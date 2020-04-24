cda46c6917fce0704e501fc5a04a99895861caea
Alexander Guschin <igushin1999@gmail.com>
2020-04-24 22:41:19 +0300

    opencv done

--- modules/task_2/guschin_a_cc_labeling/main.cpp
+++ modules/task_2/guschin_a_cc_labeling/main.cpp
@@ -25,14 +25,24 @@ void Show(const std::vector<std::vector<std::int32_t>>& v_image) {
   for (std::int32_t i = 0; i < v_image.size(); ++i)
     for (std::int32_t j = 0; j < v_image[0].size(); ++j) {
       cv::Vec3b iter_color;
-      std::int32_t color = v_image[i][j] * 100;
-      iter_color[0] = (color / 10) % 256;
-      iter_color[1] = (color / 20) % 256;
-      iter_color[2] = (color / 50) % 256;
+      if (v_image[i][j] == 0) {
+        iter_color[0] = 255;
+        iter_color[1] = 255;
+        iter_color[2] = 255;
+      } else {
+        std::int32_t color = v_image[i][j];
+        iter_color[0] = 255 - (v_image[i][j] * 10) % 255;
+            //(v_image[i][j] * v_image[i][j] * v_image[i][j]) % 256;
+        iter_color[1] = 255 - (v_image[i][j] * 20) % 255;
+            //(v_image[i][j] * v_image[i][j]) % 256;
+        iter_color[2] = 255 - (v_image[i][j] * 30) % 255;
+            //(v_image[i][j]) % 256;
+      }
       image.at<cv::Vec3b>(i, j) = iter_color;
     }
 
   imshow("Display Window", image);
+  cv::waitKey(0);
 }
 
 
@@ -41,7 +51,7 @@ std::int32_t main() {
   std::string source = "C:\\Users\\igush\\Desktop\\Paint\\test.bmp"; 
   cv::Mat Image = cv::imread(source, 1);
   std::vector<std::vector<std::int8_t>> bin_matrix = Convert(Image);
-  std::vector<std::vector<std::int32_t>> result = Labeling(bin_matrix);
+  std::vector<std::vector<std::int32_t>> result = Labeling_omp(bin_matrix);
   Show(result);
   return 0; 
 }
